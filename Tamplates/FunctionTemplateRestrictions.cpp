9850de25b18853de4c8553f1497cefb5cf43a53a
raushanjha146 <raushanjha146@gmail.com>
2020-05-02 02:05:38 +0530

    Create FunctionTemplateRestrictions.cpp

---
+++ Tamplates/FunctionTemplateRestrictions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>  
+using namespace std;  
+void fun(double a)  
+{  
+    cout<<"value of a is : "<<a<<'\n';  
+}  
+  
+void fun(int b)  
+{  
+    if(b%2==0)  
+    {  
+        cout<<"Number is even";  
+    }  
+    else  
+    {  
+        cout<<"Number is odd";  
+    }  
+  
+}  
+  
+int main()  
+{  
+   fun(4.6);  
+   fun(6);  
+   return 0;  
+}  
+
+/*
+Output : 
+
+value of a is : 4.6
+Number is even
+*/
